0ac25e3339de97d1e94b96e9bad4b48cb4fed2cc
234-maker <aryansrivastav201999@gmail.com>
2025-12-11 06:57:08 +0530

    Add files via upload
    All the codes based on JAVA language are here.

---
+++ JAVA/Thankyou.java
@@ -0,0 +1,12 @@
+
+class Help {
+    public static void main(String[] args) {
+       System.out.println("Please help."); 
+    }
+    
+}
+public class Thankyou extends Help{
+   public static void main(String[] args){
+    System.out.println("Thankyou for helping.");
+   }
+}
---
+++ JAVA/string.java
@@ -0,0 +1,8 @@
+public class string {
+    public static void main(String[] args) {
+        String str1 ="hello";
+        String str2 =" World";
+        String str3 =" I am Aryan.";
+        System.out.println(str1 + str2+str3);
+    }
+}
